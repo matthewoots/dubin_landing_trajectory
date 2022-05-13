7a1d2b1611ff15a63a3ad4da57d138a27451b237
matthewoots <matthewoots@gmail.com>
2022-05-13 21:39:19 +0800

    Added planar circle tracker function that returns flags for whether the next point is valid

--- functions/planar_circle_tracker.m
+++ functions/planar_circle_tracker.m
@@ -1,59 +1,72 @@
 function [target, flag] = planar_circle_tracker(C, P, radius, tolerance, rot, limit_pair)    
+    deg_to_rad = 1/180 * pi;
+    rad_to_deg = 1/pi * 180;
+
+    % Make sure this aligns to clockwise or anticlockwise
+    % The pair consist of [start end] angle
+    % This is the output from dubin flight path
+    fprintf('[ct] limit_pair (%.3f %.3f)\n', limit_pair(1), limit_pair(2));
+
     %% Find nearest point on the circle perimeter closest to the point
     % https://stackoverflow.com/questions/300871/best-way-to-find-a-point-on-a-circle-closest-to-a-given-point
-    pc = C - P; % point to center
-    pc_norm_2 = sqrt(pc(1)^2 + pc(2)^2);
-    np = C + (pc / pc_norm_2) * radius; % Nearest point on the circle
-    
-    %% Find nearest point is inside or outside of the circle
-    pnp = np - p;
-    % Case is inside of circle
-    if pc_norm_2 < radius
-        % ctp will be a
-        ctp_first = true;
-        
-    elseif mag >= radius
-        % Outside Circle
-        % ctp will be b
-        ctp_first = false;
-        
-    end
-    
-    % Clockwise direction is needed
-    % Inside = p_ctp -> p_np
-    % Outside = p_np -> p_ctp 
-    % Use clockwise dot product
-    
-    angle = 0;
-    next_idx = idx - 1;
-    while angle < tolerance
-        next_idx = next_idx + 1;
-        curr_track_p = wp(idx, :);
-        
-        p_ctp = P - curr_track_p;
-        dot_num = pnp(1) * p_ctp(1) + pnp(2) * p_ctp(2);
-        dot_den = norm(pnp) * norm(p_ctp);    
+    cp = P - C; % center to point
+    cp_norm_2 = norm(cp);
+    np = C + (cp / cp_norm_2) * radius; % Nearest point on the circle
 
-        angle = acos(dot_num / dot_den);
-        
-        % a x b since we need to know in comparison 
-        % which is clockwise and anticlockwise
-        if ctp_first
-            sign_value =  p_ctp(1) * pnp(2) - p_ctp(2) * pnp(1);
-        elseif ~ctp_first
-            sign_value =  pnp(1) * p_ctp(2) - pnp(2) * p_ctp(1);
+    %% Find the next point
+    % Counter-clockwise is positive, clockwise is negative
+    dir = (strcmp(rot,'cw') - 0.5) / 0.5; % Produces -1 or 1
+    cir_ang_change = tolerance / radius;
+
+    % Find current point's angle
+    % vect1 is facing y (cf) and vect2 is facing current angle (cp)
+    cf = [0 radius 0];
+    % https://stackoverflow.com/questions/14066933/direct-way-of-computing-clockwise-angle-between-2-vectors
+    % Find angle between 2 vectors
+    dotp = dot(cf, cp); % dot product between [x1, y1] and [x2, y2]
+    detp = cf(2)*cp(1) - cp(2)*cf(1); % determinant
+    curr_ang_rot = atan2(detp, dotp);
+    fprintf('[ct] cir_ang_change %.3f deg\n',cir_ang_change/pi * 180);
+    fprintf('[ct] curr_ang_rot %.3f deg\n',curr_ang_rot/pi * 180);
+    next_ang = wrapToPi(curr_ang_rot + dir * cir_ang_change);
+    next_point = [C(1) + radius * sin(next_ang), ...
+                C(2) + radius * cos(next_ang), P(3)];
+
+    %% Check that [next_point] is within the limits from dubin path
+    % For clockwise (cw) final - initial will yield a positive value
+    % For counter-clockwise (ccw) -(final - initial) will yield a positive value
+    if strcmp(rot,'cw')
+        angle_length = limit_pair(2) - limit_pair(1);
+        wrap_check = (limit_pair(2) - limit_pair(1)) > 0;
+        if ~wrap_check
+            angle_length = angle_length + 2 * pi;
         end
-        
-        if sign_value == 0
-            % There is no rotation
-            angle = angle;
-        else
-            sign = sign_value / abs(sign_value);
-            angle = sign * angle;
+
+        angle_length_path = next_ang - limit_pair(1);
+        wrap_check_path = (next_ang - limit_pair(1)) > 0;
+        if ~wrap_check_path
+            angle_length_path = angle_length_path + 2 * pi;
         end
-            
-         
+    else
+        angle_length = -(limit_pair(2) - limit_pair(1));
+        wrap_check = -(limit_pair(2) - limit_pair(1)) > 0;
+        if ~wrap_check
+            angle_length = angle_length + 2 * pi;
+        end
+
+        angle_length_path = -(next_ang - limit_pair(1));
+        wrap_check_path = -(next_ang - limit_pair(1)) > 0;
+        if ~wrap_check_path
+            angle_length_path = angle_length_path + 2 * pi;
+        end
     end
+
+    fprintf('[ct] angle_length %.3fdeg\n',angle_length * rad_to_deg);
+
+    % Check with the next angle [next_ang]
+    flag = (angle_length - angle_length_path) > 0;
+    target = next_point;
+    fprintf('[ct] flag %d\n',flag);
     
 end
 
--- scipts/b2_circle_tracker_function.m
+++ scipts/b2_circle_tracker_function.m
@@ -58,69 +58,10 @@ start_dubin = [C(1) + radius * sin(limit_pair(1)), ...
 end_dubin = [C(1) + radius * sin(limit_pair(2)), ...
             C(2) + radius * cos(limit_pair(2)), P(3)];
 fprintf('[ct] limit_pair (%.3f %.3f)\n', limit_pair(1), limit_pair(2));
-    
-%% Find nearest point on the circle perimeter closest to the point
-% https://stackoverflow.com/questions/300871/best-way-to-find-a-point-on-a-circle-closest-to-a-given-point
-cp = P - C; % center to point
-cp_norm_2 = norm(cp);
-np = C + (cp / cp_norm_2) * radius; % Nearest point on the circle
-
-%% Find the next point
-% Counter-clockwise is positive, clockwise is negative
-dir = (strcmp(rot,'cw') - 0.5) / 0.5; % Produces -1 or 1
-cir_ang_change = tolerance / radius;
-
-% Find current point's angle
-% vect1 is facing y (cf) and vect2 is facing current angle (cp)
-cf = [0 minTurnRad 0];
-% https://stackoverflow.com/questions/14066933/direct-way-of-computing-clockwise-angle-between-2-vectors
-% Find angle between 2 vectors
-dotp = dot(cf, cp); % dot product between [x1, y1] and [x2, y2]
-detp = cf(2)*cp(1) - cp(2)*cf(1); % determinant
-curr_ang_rot = atan2(detp, dotp);
-fprintf('[ct] cir_ang_change %.3f deg\n',cir_ang_change/pi * 180);
-fprintf('[ct] curr_ang_rot %.3f deg\n',curr_ang_rot/pi * 180);
-next_ang = wrapToPi(curr_ang_rot + dir * cir_ang_change);
-next_point = [C(1) + radius * sin(next_ang), ...
-            C(2) + radius * cos(next_ang), P(3)];
+[next_point, flag] = planar_circle_tracker(C, P, radius, tolerance, rot, limit_pair);
 
 % To plot the track line from current to next point
 track_line = [p; next_point];
-
-%% Check that [next_point] is within the limits from dubin path
-% For clockwise (cw) final - initial will yield a positive value
-% For counter-clockwise (ccw) -(final - initial) will yield a positive value
-if strcmp(rot,'cw')
-    angle_length = limit_pair(2) - limit_pair(1);
-    wrap_check = (limit_pair(2) - limit_pair(1)) > 0;
-    if ~wrap_check
-        angle_length = angle_length + 2 * pi;
-    end
-    
-    angle_length_path = next_ang - limit_pair(1);
-    wrap_check_path = (next_ang - limit_pair(1)) > 0;
-    if ~wrap_check_path
-        angle_length_path = angle_length_path + 2 * pi;
-    end
-else
-    angle_length = -(limit_pair(2) - limit_pair(1));
-    wrap_check = -(limit_pair(2) - limit_pair(1)) > 0;
-    if ~wrap_check
-        angle_length = angle_length + 2 * pi;
-    end
-    
-    angle_length_path = -(next_ang - limit_pair(1));
-    wrap_check_path = -(next_ang - limit_pair(1)) > 0;
-    if ~wrap_check_path
-        angle_length_path = angle_length_path + 2 * pi;
-    end
-end
-
-fprintf('[ct] angle_length %.3fdeg\n',angle_length * rad_to_deg);
-
-% Check with the next angle [next_ang]
-flag = (angle_length - angle_length_path) > 0;
-fprintf('[ct] flag %d\n',flag);
 
 
 %% Draw the Circle
@@ -137,7 +78,6 @@ hold on
 plot3(c1(:,1),c1(:,2),h,'--','DisplayName','perimeter');
 plot3(nCi(1),nCi(2), nCi(3),'x','DisplayName','center');
 plot3(p(1,1),p(1,2),p(1,3),'o','DisplayName','random point');
-plot3(np(1,1),np(1,2),np(1,3),'x','MarkerSize',5,'DisplayName','nearest point');
 plot3(start_dubin(1,1),start_dubin(1,2),start_dubin(1,3), ...
     'o','DisplayName','start point');
 plot3(end_dubin(1,1),end_dubin(1,2),end_dubin(1,3), ...
